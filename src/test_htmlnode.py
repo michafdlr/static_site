6e9cc35059e2bb6a691cca9938c1224c045fb58f
Michael Fiedler <michael.fiedler1604@gmail.com>
2024-03-28 11:13:10 +0100

    Added LeafNode class and unittest

--- src/test_htmlnode.py
+++ src/test_htmlnode.py
@@ -1,6 +1,6 @@
 import unittest
 
-from htmlnode import HTMLNode
+from htmlnode import HTMLNode, LeafNode
 
 class TestHTMLNode(unittest.TestCase):
     def test_propstohtml(self):
@@ -15,12 +15,26 @@ class TestHTMLNode(unittest.TestCase):
             "target": "not_blank",
             "something": "other"}
             )
+
+        leafnode1 = LeafNode("p", "This is a paragraph of text.")
+        leafnode2 = LeafNode("a", "Click me!", {"href": "https://www.google.com"})
+
         self.assertEqual(node.props_to_html(),
                          ' href="https://www.google.com" target="_blank"'
                          )
         self.assertEqual(node2.props_to_html(),
                          ' href="https://www.google.com" target="not_blank" something="other"'
                          )
+
+        self.assertEqual(
+            leafnode1.to_html(),
+            '<p>This is a paragraph of text.</p>'
+        )
+        self.assertEqual(
+            leafnode2.to_html(),
+            '<a href="https://www.google.com">Click me!</a>'
+        )
+
 
 
 
